b30ba75c1bbb6a515fb4294796eb57c64f1163d0
rajkh1138 <rajkh@mail.com>
2025-03-23 13:04:45 +0530

    Fixed GitHub Pages issue and updated project

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/my-youtube-analyzer/", // Yeh path update kar
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
